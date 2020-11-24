eacb5e5a77ae3b045a2883e3b95dee075248ba6e
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-11-24 03:24:23 +0000

    Added string literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 //Function Implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("\u2705 \u2705 \u2705 Assertion Passed: " + actual + " === " + expected);
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("\u26D4 \u26D4 \u26D4 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`\u26D4 \u26D4 \u26D4 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
