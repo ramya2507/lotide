52ab273a071f1990fa40f5438ea761915d79705e
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-11-25 23:59:09 +0000

    Function to return the key matching the argument value passed

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 \u26D4 \u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Function to return the key matching the argument value passed
+const findKeyByValue = (givenObject,value) => {
+  for(let key in givenObject){
+    if(givenObject[key] === value){
+     return key;
+    } 
+  }
+};
+
+//Test code
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
