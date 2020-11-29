43e2c30028ae0fff3a79484f495a0372770bc202
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-11-29 04:32:50 +0000

    Refactored middle and wrote unit test case

--- middle.js
+++ middle.js
@@ -1,26 +1,4 @@
-const eqArrays = (array1,array2) => {
-  if(array1.length === array2.length){
-    for(let i = 0; i < array1.length; i++){
-      if(array1[i] === array2[i]){
-
-      }else{
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-//function that consoles the comparison result of two arrays
-const assertArraysEqual =  (actual,expected) => {
-  let result = eqArrays(actual,expected);
-  if (result) {
-    console.log(`\u2705 \u2705 \u2705 Assertion Passed!`);
-  } else {
-    console.log(`\u26D4 \u26D4 \u26D4 Assertion Failed!`);
-  }
-};
+const assertArraysEqual =  require('./assertArraysEqual');
 
 //function to return the mid elements of an array
 const middle = (array) => {
@@ -39,9 +17,4 @@ const middle = (array) => {
     return [];
   } 
 };
-//Test code
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+ module.exports = middle;
